5b9143e09a7c6eea0d909e9fb0307715b1a640ab
test <mglumac@gmail.com>
2013-09-26 12:52:41 -0400

    removing files

--- db/migrate/20130924233811_add_column_to_worduses.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnToWorduses < ActiveRecord::Migration
-  def up
-    add_column :worduses, :id, :primary_key
-  end
-
-  def down
-    add_column :worduses, :id, :primary_key
-  end
-end
